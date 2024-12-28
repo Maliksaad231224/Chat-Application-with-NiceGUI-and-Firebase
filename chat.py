aac038b1048d1892e506752b9524655843b8253c
saad <awansaad6927@gmail.com>
2024-12-28 20:31:28 +0500

    fixed chat

--- chat.py
+++ chat.py
@@ -1,40 +1,48 @@
+#!/usr/bin/env python3
+from datetime import datetime
+from typing import List, Tuple
 from uuid import uuid4
+
 from nicegui import ui
-import os
 
-image_path = 'image/login-bg.png'
+messages: List[Tuple[str, str, str, str]] = []
 
-# Check if the background image exists, then apply it as a background
-if os.path.exists(image_path):
-    ui.image(image_path).style("position: fixed; top: 0; left: 0; width: 100%; height: 100%; object-fit: cover; z-index: -1;")
-else:
-    print("Error: Image not found at the specified path")
-
-messages = []
 
 @ui.refreshable
-def chat_messages(own_id):
-    for user_id, avatar, text in messages:
-        ui.chat_message(avatar=avatar, text=text, sent=user_id==own_id)
+def chat_messages(own_id: str) -> None:
+    if messages:
+        for user_id, avatar, text, stamp in messages:
+            ui.chat_message(text=text, stamp=stamp, avatar=avatar, sent=own_id == user_id)
+    else:
+        ui.label('No messages yet').classes('mx-auto my-36')
+    ui.run_javascript('window.scrollTo(0, document.body.scrollHeight)')
+
 
 @ui.page('/')
-def index():
-    def send():
-        messages.append((user, avatar, text.value))
-        chat_messages.refresh()
+async def main():
+    def send() -> None:
+        stamp = datetime.now().strftime('%X')
+        messages.append((user_id, avatar, text.value, stamp))
         text.value = ''
+        chat_messages.refresh()
 
-    user = str(uuid4())
-    avatar = f'https://robohash.org/{user}?bgset=bg2'
-    with ui.column().classes('w-full items-stretch'):
-        chat_messages(user)
+    user_id = str(uuid4())
+    avatar = f'https://robohash.org/{user_id}?bgset=bg2'
 
-    with ui.footer().classes('bg-white'):
-        with ui.row().classes('w-full items-center'):
-            with ui.avatar():
+    ui.add_css(r'a:link, a:visited {color: inherit !important; text-decoration: none; font-weight: 500}')
+    with ui.footer().classes('bg-white'), ui.column().classes('w-full max-w-3xl mx-auto my-6'):
+        with ui.row().classes('w-full no-wrap items-center'):
+            with ui.avatar().on('click', lambda: ui.navigate.to(main)):
                 ui.image(avatar)
-            text = ui.input(placeholder='message') \
-                .props('rounded outlined').classes('flex-grow') \
-                .on('keydown.enter', send)
+            text = ui.input(placeholder='message').on('keydown.enter', send) \
+                .props('rounded outlined input-class=mx-3').classes('flex-grow')
+        ui.markdown('simple chat app built with [NiceGUI](https://nicegui.io)') \
+            .classes('text-xs self-end mr-8 m-[-1em] text-primary')
 
-ui.run()
+    await ui.context.client.connected()  # chat_messages(...) uses run_javascript which is only possible after connecting
+    with ui.column().classes('w-full max-w-2xl mx-auto items-stretch'):
+        chat_messages(user_id)
+
+
+if __name__ in {'__main__', '__mp_main__'}:
+    ui.run(port=8007)
